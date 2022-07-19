44bae28700f77eca0d82b3526919beba0779d65b
fltonii <tonialjoao97@gmail.com>
2022-07-18 22:23:16 -0300

    ugly but works

--- server.mjs
+++ server.mjs
@@ -1,19 +1,55 @@
-import { createServer } from "http";
 import fetch from "node-fetch";
 import express from "express";
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
+const apiURL = "https://api.coingecko.com/api/v3/";
+
+const getInfo = async (id) => {
+  const url =
+    apiURL +
+    `coins/${id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`;
+  const apiResponse = await fetch(url);
+  const json = await apiResponse.json();
+  const {
+    price_change_24h,
+    price_change_percentage_24h,
+    current_price: { usd },
+  } = json.market_data;
+  return { price_change_24h, price_change_percentage_24h, price: usd };
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+app.get("/daang/:symbol/:action", async (req, res) => {
+  console.log(req.params);
+  const { symbol, action } = req.params;
+
+  try {
+    const apiResponse = await fetch(apiURL + "search?query=" + symbol);
+    const json = await apiResponse.json();
+
+    const selectedToken = json.coins
+      .filter(
+        (token) => token.symbol.toUpperCase() === (symbol || "").toUpperCase()
+      )
+      .pop();
+
+    const id = selectedToken.id;
+
+    const info = await getInfo(id);
+
+    res.send(JSON.stringify(info[action]));
+  } catch (error) {
+    console.log(error);
+    res.send("broken");
+  }
 });
 
-// createServer((req, res) => {
-//   fetch("https://api.coingecko.com/api/v3/search?query=btc");
-//   res.write("Hello World!");
-//   res.end();
-// }).listen();
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
