8c45884343107ceffca4366d40d3ba005fb49c2c
fltonii <tonialjoao97@gmail.com>
2022-07-18 21:23:29 -0300

    express

--- server.mjs
+++ server.mjs
@@ -1,6 +1,19 @@
-import { createServer } from 'http';
+import { createServer } from "http";
+import fetch from "node-fetch";
+import express from "express";
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
+
+// createServer((req, res) => {
+//   fetch("https://api.coingecko.com/api/v3/search?query=btc");
+//   res.write("Hello World!");
+//   res.end();
+// }).listen();
